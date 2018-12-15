d41d712ad25789b10b57a5aacf99f7f10d69483b
hatobus <s1240056@gmail.com>
2018-12-16 08:57:17 +0900

    /pub/gun/shotのひとまずのコード

---
+++ Raspi/gunpub.py
@@ -0,0 +1,29 @@
+# /pub/gun/shotで送信するところ
+import paho.mqtt.client as mqtt
+import json
+import random
+import time
+
+
+host = "192.168.179.7"
+port = 1883
+topic = "/pub/gun/shot"
+
+client = mqtt.Client(protocol=mqtt.MQTTv311)
+
+client.connect(host, port=port, keepalive=10)
+
+pubdict = {"topic": ""}
+
+# 最初の方だったのでランダムでjsonを生成してました
+def choice():
+    return random.choice(["red", "clap"])
+
+for i in range(10):
+    print(i)
+    pubdict["topic"] = choice()
+    jsn_str = json.dumps(pubdict)
+    client.publish(topic, jsn_str)
+    time.sleep(0.5)
+
+client.disconnect()
