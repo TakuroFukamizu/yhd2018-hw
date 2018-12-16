5f059a05db33d798fc0328e3da29d03acf5634c8
hatobus <s1240056@gmail.com>
2018-12-16 10:51:39 +0900

    attach shotpub

--- Raspi/shotpub.py
+++ Raspi/shotpub.py
@@ -1,17 +1,22 @@
 import paho.mqtt.client as mqtt
 import json
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 host = "192.168.179.7"
 port = 1883
 topic = "/pub/gun/shot"
 # クラッピー追記お願いします
 names = {
-    "赤べこ": "akabeko"
+    8: "akabeko",
+    0: "red",
+    1: "yellow"
 }
 
 def on_connect(client, userdata, flags, respons_code):
     print('status {0}'.format(respons_code))
-    trigger()
 
 def load_buffer():
     head = 0
@@ -39,12 +44,18 @@ def trigger():
     for data in dataList:
         # 名前を解決
         pubdata = {
-            "target": names[json.loads(data)["items"][0]["name"]]
+            "target": names[json.loads(data)["items"][0]["id"]]
         }
         client.publish(topic, json.dumps(pubdata))
+
+def buttonstate():
+    while True:
+        if GPIO.input(24) == True:
+            trigger()
+
 
 if __name__ == '__main__':
     client = mqtt.Client(protocol=mqtt.MQTTv311)
     client.on_connect = on_connect
     client.connect(host, port=port, keepalive=10)
-    client.loop_forever()
+    buttonstate()
